1d60b31bb642d0a8ab5383194a016aa3a1791fb7
vmpenyana <vincent@tenxrs.com>
2023-08-31 14:17:48 +0200

    Initializing the project.

---
+++ src/main/java/com/tenxrs/tktbackend/TktBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tenxrs.tktbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TktBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TktBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenxrs/tktbackend/TktBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenxrs.tktbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TktBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
