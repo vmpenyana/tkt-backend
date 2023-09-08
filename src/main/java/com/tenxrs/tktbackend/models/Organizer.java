50f1fd8f29a84379fa1802444ab85d6c20f9081c
vmpenyana <vincent@tenxrs.com>
2023-09-08 21:03:30 +0200

    Converted some POJOs to Spring Data JPA entities.

--- src/main/java/com/tenxrs/tktbackend/models/Organizer.java
+++ src/main/java/com/tenxrs/tktbackend/models/Organizer.java
@@ -1,11 +1,21 @@
 package com.tenxrs.tktbackend.models;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "organizers")
 public class Organizer {
-    private String organizer;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
     private String emailAddress;
     private long cellNumber;
+    @OneToOne
+    private BankingDetails bankingDetails;
 
 }
