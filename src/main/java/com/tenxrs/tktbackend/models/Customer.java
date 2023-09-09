39bbb0b161db8e20b3d27a71f047dc546b8a656c
Nick Mpenyana <nickmpenyana@gmail.com>
2023-09-09 09:02:59 +0200

    converted the remaining pojo's to spring data JPA

--- src/main/java/com/tenxrs/tktbackend/models/Customer.java
+++ src/main/java/com/tenxrs/tktbackend/models/Customer.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 @Data
 @Entity
+@Table(name = "customers")
 public class Customer {
 
     @Id
--- src/main/java/com/tenxrs/tktbackend/models/Sale.java
+++ src/main/java/com/tenxrs/tktbackend/models/Sale.java
@@ -1,8 +1,15 @@
 package com.tenxrs.tktbackend.models;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "sales")
 public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
     private Event event;
 }
