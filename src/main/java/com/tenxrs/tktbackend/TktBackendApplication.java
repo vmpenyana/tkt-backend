7843ab78fca0613d7b6aefb42c78c99fa2a92afb
vmpenyana <vincent@tenxrs.com>
2023-09-11 20:25:42 +0200

    Removed unused stuff from Application class.

--- src/main/java/com/tenxrs/tktbackend/TktBackendApplication.java
+++ src/main/java/com/tenxrs/tktbackend/TktBackendApplication.java
@@ -1,12 +1,7 @@
 package com.tenxrs.tktbackend;
 
-import com.tenxrs.tktbackend.models.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.HashSet;
-import java.util.Set;
-
 @SpringBootApplication
 public class TktBackendApplication {
 
